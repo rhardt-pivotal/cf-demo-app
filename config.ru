a62dacc9128f3eb1b80da1a02587ee61b011df18
Rob Hardt <rhardt@pivotal.io>
2016-09-23 14:42:14 -0700

    initial commit

---
+++ config.ru
@@ -0,0 +1,4 @@
+$stdout.sync = true
+require './scaledemo'
+run Sinatra::Application
+
---
+++ scaledemo.rb
@@ -0,0 +1,97 @@
+
+  require 'rubygems'
+  require 'sinatra'
+  require 'yaml'
+  require 'json'
+  require 'net/http'
+
+  set :requests, 0
+  set :bind, '0.0.0.0'
+  set :protection, :except => :frame_options
+
+  ev = JSON.pretty_generate(ENV.to_hash)
+
+  get '/' do
+     settings.requests += 1
+     app_info = ENV['VCAP_APPLICATION'] ? JSON.parse(ENV['VCAP_APPLICATION']) : Hash.new
+     ENV['APP_NAME'] = app_info["application_name"] ? app_info["application_name"] : "unknown"
+     ENV['APP_INSTANCE'] = app_info["instance_index"] ? app_info["instance_index"].to_s : "unknown"
+     ENV['APP_MEM'] = app_info["limits"] ? app_info["limits"]["mem"].to_s : " "
+     ENV['APP_DISK'] = app_info["limits"] ? app_info["limits"]["disk"].to_s : " "
+     ENV['APP_IP'] = IPSocket.getaddress(Socket.gethostname)
+     ENV['APP_PORT'] = app_info["port"] ? app_info["port"].to_s : "unknown"
+     ENV['SERVICE_JSON'] = ENV['VCAP_SERVICES'] ? JSON.pretty_generate(JSON.parse(ENV['VCAP_SERVICES'])) : "n/a"
+     ENV['EVERYTHING'] = ev
+
+     erb :'index'
+  end
+
+  get '/tile' do
+    settings.requests += 1
+    app_info = ENV['VCAP_APPLICATION'] ? JSON.parse(ENV['VCAP_APPLICATION']) : Hash.new
+    ENV['APP_NAME'] = app_info["application_name"] ? app_info["application_name"] : "unknown"
+    ENV['APP_INSTANCE'] = app_info["instance_index"] ? app_info["instance_index"].to_s : "-1"
+    ENV['REQ_COUNT'] = settings.requests.to_s
+    services = ENV['VCAP_SERVICES'] ? JSON.parse(ENV['VCAP_SERVICES']) : {}
+    if services['cleardb'] or services['p-mysql'] then
+      ENV['DB_HTML'] = "<img src='/img/db.png' width='32px' height='32px' alt='db'></img>"
+    end
+    if ENV['OCC_BUG'] then
+      r = Random.new
+      x = r.rand(10)
+      if x > 6 then
+        ENV['BUG_HTML'] = "<img src='/img/bug.png' width='32px' heigh='32px' alt='bug'></img>"
+      else
+        ENV['BUG_HTML'] = ""
+      end
+
+    end
+
+
+    erb :'index2'
+  end
+
+
+  get '/tiles' do
+    erb :'tiles'
+  end
+
+  get '/tile-demo' do
+    erb :'tile-demo'
+  end
+
+
+  get '/killSwitch' do
+    Kernel.exit!
+  end
+
+  get '/load' do
+    i = 0
+    puts (File.read("banner.txt"))
+    myStr = "Kill the CPU!!!"
+    buff = ""
+    while i < 50000  do
+      buff += myStr.to_s
+      buff.reverse!
+      i += 1
+    end
+    settings.requests += 1
+    "<h2>I'm healthy2!</h2>"
+  end
+
+  get '/health' do
+    settings.requests += 1
+    "<h2>I'm healthy!</h2>"
+  end
+
+
+  get '/greet/:name/:num' do
+    g = {'name' => params['name'], 'num' => params['num']}
+    g.to_json
+
+  end
+
+  def colorize(string, color_code)
+    "\e[#{color_code}m#{string}\e[0m"
+  end
+
